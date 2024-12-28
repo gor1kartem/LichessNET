d2e8fb69d7be384aea5e5ae073f5464d6635368c
Raberger Raphael Matthias <raphael.raberger@outlook.com>
2024-12-28 11:14:16 +0100

    feat(team management): Added endpoints to join and leave a team.

--- LichessNET/API/TeamAPI.cs
+++ LichessNET/API/TeamAPI.cs
@@ -1,5 +1,6 @@
 ﻿using LichessNET.Entities.Teams;
 using LichessNET.Entities.Tournament;
+using LichessNET.Entities.Tournament.Arena;
 using Newtonsoft.Json;
 
 namespace LichessNET.API;
@@ -23,6 +24,11 @@ public partial class LichessApiClient
         return team;
     }
 
+    /// <summary>
+    /// Gets the most populat teams on Lichess in a paginated manner.
+    /// </summary>
+    /// <param name="page">Page number, defaults to 1</param>
+    /// <returns>The list of the most popular teams on the specified page.</returns>
     public async Task<List<LichessTeam>> GetPopularTeamsAsync(int page = 1)
     {
         _ratelimitController.Consume();
@@ -100,5 +106,86 @@ public partial class LichessApiClient
         }
 
         return tournaments;
+    }
+
+    /// <summary>
+    /// Retrieves all Arena tournaments relevant to a team.
+    /// </summary>
+    /// <param name="teamId">The ID of the team.</param>
+    /// <param name="max">The maximum number of tournaments to download. Default is 100.</param>
+    /// <returns>A task representing the asynchronous operation, containing the list of Arena tournaments.</returns>
+    public async Task<List<ArenaTournament>> GetTeamArenaTournamentsAsync(string teamId, int max = 100)
+    {
+        _ratelimitController.Consume();
+
+        var endpoint = $"api/team/{teamId}/arena?max={max}";
+        var request = GetRequestScaffold(endpoint);
+
+        var response = await SendRequest(request);
+        var content = await response.Content.ReadAsStringAsync();
+
+        var tournaments = new List<ArenaTournament>();
+        using (var reader = new StringReader(content))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var tournament = JsonConvert.DeserializeObject<ArenaTournament>(line);
+                tournaments.Add(tournament);
+            }
+        }
+
+        return tournaments;
+    }
+
+    /// <summary>
+    /// Joins a team.
+    /// </summary>
+    /// <param name="teamId">The ID of the team.</param>
+    /// <param name="message">The message for the team join request, required if the team manually reviews admission requests.</param>
+    /// <param name="password">The password for the team, if required.</param>
+    /// <returns>A task representing the asynchronous operation, containing the join team response.</returns>
+    public async Task<bool> JoinTeamAsync(string teamId, string message = null, string password = null)
+    {
+        _ratelimitController.Consume();
+
+        var endpoint = $"team/{teamId}/join";
+        var request = GetRequestScaffold(endpoint);
+
+        var formData = new Dictionary<string, string>();
+        if (!string.IsNullOrEmpty(message))
+        {
+            formData.Add("message", message);
+        }
+
+        if (!string.IsNullOrEmpty(password))
+        {
+            formData.Add("password", password);
+        }
+
+        request.Content = new FormUrlEncodedContent(formData);
+
+        var response = await SendRequest(request, HttpMethod.Post);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content.Contains("true");
+    }
+
+    /// <summary>
+    /// Leaves a team.
+    /// </summary>
+    /// <param name="teamId">The ID of the team.</param>
+    /// <returns>A task representing the asynchronous operation, containing the leave team response.</returns>
+    public async Task<bool> LeaveTeamAsync(string teamId)
+    {
+        _ratelimitController.Consume();
+
+        var endpoint = $"team/{teamId}/quit";
+        var request = GetRequestScaffold(endpoint);
+
+        var response = await SendRequest(request, HttpMethod.Post);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content.Contains("true");
     }
 }
