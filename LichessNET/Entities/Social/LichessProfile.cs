d0f855b751ae16067c9f179491d4c4039e34a8dc
gor1kartem <gorikartem553@gmail.com>
2025-02-18 17:32:27 +0300

    Made all fields nullable

--- LichessNET/Entities/Social/LichessProfile.cs
+++ LichessNET/Entities/Social/LichessProfile.cs
@@ -8,51 +8,51 @@ public class LichessProfile
     /// <summary>
     ///     The current country flag of the user
     /// </summary>
-    public string Flag { get; set; }
+    public string? Flag { get; set; }
 
     /// <summary>
     ///     The set location of this user
     /// </summary>
-    public string Location { get; set; }
+    public string? Location { get; set; }
 
     /// <summary>
     ///     The bio of the user
     /// </summary>
-    public string Bio { get; set; }
+    public string? Bio { get; set; }
 
     /// <summary>
     ///     The set real name of the user
     /// </summary>
-    public string RealName { get; set; }
+    public string? RealName { get; set; }
 
     /// <summary>
     ///     FIDE rating of the user
     /// </summary>
-    public ushort FideRating { get; set; }
+    public ushort? FideRating { get; set; }
 
     /// <summary>
     ///     USCF rating of the user
     /// </summary>
-    public ushort UsCfRating { get; set; }
+    public ushort? UsCfRating { get; set; }
 
     /// <summary>
     ///     ECF rating of the user
     /// </summary>
-    public ushort EcfRating { get; set; }
+    public ushort? EcfRating { get; set; }
 
     /// <summary>
     ///     CFC rating of the user
     /// </summary>
-    public ushort CfcRating { get; set; }
+    public ushort? CfcRating { get; set; }
 
     /// <summary>
     ///     DSB rating of the user
     /// </summary>
-    public ushort DsbRating { get; set; }
+    public ushort? DsbRating { get; set; }
 
     /// <summary>
     ///     Links mentioned in the bio of the user
     ///     Each link is seperated by \r\n
     /// </summary>
-    public string Links { get; set; }
+    public string? Links { get; set; }
 }
