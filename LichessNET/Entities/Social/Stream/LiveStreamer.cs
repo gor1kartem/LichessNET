63ea3c8968d85ce4f97713cef170fc3ec6de5c5e
gor1kartem <gorikartem553@gmail.com>
2025-03-23 18:25:06 +0300

    Added Title enum to LiveStreamer.cs

--- LichessNET/Entities/Social/Stream/LiveStreamer.cs
+++ LichessNET/Entities/Social/Stream/LiveStreamer.cs
@@ -1,10 +1,14 @@
-﻿namespace LichessNET.Entities.Social.Stream;
+﻿using System.Text.Json.Serialization;
+using LichessNET.Entities.Enumerations;
+
+namespace LichessNET.Entities.Social.Stream;
 
 public class LiveStreamer
 {
     public string Id { get; set; }
     public string Name { get; set; }
-    public string Title { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public Title Title { get; set; }
     public bool Patron { get; set; }
     public Stream Stream { get; set; }
     public Streamer Streamer { get; set; }
