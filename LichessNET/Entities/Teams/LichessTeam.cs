6cf83904bed51e596f5a560ad5efc28e8403e39a
gor1kartem <gorikartem553@gmail.com>
2025-03-25 18:27:00 +0300

    Replaced TeamLeader with UserOverview, they are identical

--- LichessNET/Entities/Teams/LichessTeam.cs
+++ LichessNET/Entities/Teams/LichessTeam.cs
@@ -11,8 +11,8 @@ public class LichessTeam
     public string Name { get; set; }
     public string Description { get; set; }
     public string Flair { get; set; }
-    public TeamLeader Leader { get; set; }
-    public List<TeamLeader> Leaders { get; set; }
+    public UserOverview Leader { get; set; }
+    public List<UserOverview> Leaders { get; set; }
     public int NbMembers { get; set; }
     public bool Open { get; set; }
     public bool Joined { get; set; }
