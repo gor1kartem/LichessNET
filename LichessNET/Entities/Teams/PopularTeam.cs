f3d128f36bca18b209fc584db3759d0c5e2a2192
gor1kartem <gorikartem553@gmail.com>
2025-03-25 18:29:18 +0300

    Created PopularTeam class, GetPopularTeams API does not send information whether you have joined the team or not

---
+++ LichessNET/Entities/Teams/PopularTeam.cs
@@ -0,0 +1,16 @@
+﻿using LichessNET.Entities.Social;
+
+namespace LichessNET.Entities.Teams;
+
+public class PopularTeam
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool Open { get; set; }
+    public string Flair { get; set; }
+    public int NbMembers { get; set; }
+    public UserOverview Leader { get; set; }
+    public List<UserOverview> Leaders { get; set; }
+    
+}
